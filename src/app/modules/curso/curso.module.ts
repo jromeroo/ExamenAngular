35fbe2b8e4ae1b110ce326df92cc65f6a79ab431
jromeroo <31375697+jromeroo@users.noreply.github.com>
2021-01-10 11:37:02 -0500

    Tercero: Container y Footer

--- src/app/modules/curso/curso.module.ts
+++ src/app/modules/curso/curso.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CursoRoutingModule } from './curso-routing.module';
 import { CursoComponent } from './pages/curso/curso.component';
+import { CursoContainerComponent } from './components/curso-container/curso-container.component';
+import { InstructorComponent } from './components/instructor/instructor.component';
+import { InstructorContainerComponent } from './components/instructor-container/instructor-container.component';
+import { CursoDetailPageComponent } from './pages/curso-detail-page/curso-detail-page.component';
 
 
 @NgModule({
-  declarations: [CursoComponent],
+  declarations: [CursoComponent, CursoContainerComponent, InstructorComponent, InstructorContainerComponent, CursoDetailPageComponent],
   imports: [
     CommonModule,
     CursoRoutingModule
